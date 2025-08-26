1c749db1d086c9955f099e50224f90efc1616910
asraf ali <moha.asrafali@gmail.com>
2025-08-26 21:48:16 +0600

    update

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,7 +3,6 @@ import Layout from "../components/Layout";
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
 import Contact from "../pages/Contact/Contact";
-import { Cars } from "../pages/Cars/Cars";
 import { Services } from "../pages/Services/Services";
 import ServiceDetails from "../pages/Services/ServiceDetails";
 import Team from "../pages/About/Sections/Team";
@@ -20,7 +19,6 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <Home /> },
       { path: "/about", element: <About /> },
-      { path: "/cars", element: <Cars /> },
       { path: "/services", element: <Services /> },
       { path: "/services/:slug", element: <ServiceDetails /> },
       { path: "/doctors", element: <Team /> },
